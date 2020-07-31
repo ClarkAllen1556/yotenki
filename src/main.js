c53c430e252ac10f8ef3c2c209dbd40b3d734038
Allen <allenclark@u.boisestate.edu>
2020-07-31 13:04:31 +0900

    init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import './styles/custom.scss'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+
 Vue.config.productionTip = false
 
 new Vue({
