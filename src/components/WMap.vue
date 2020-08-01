e2a98a4a46f2d825ccfac65fa9f795243390a782
Allen <allenclark@u.boisestate.edu>
2020-08-01 13:07:57 +0900

    weather map now showing correcctly

--- src/components/WMap.vue
+++ src/components/WMap.vue
@@ -8,8 +8,8 @@
       @update:center="centerUpdated"
       @update:bounds="boundsUpdated"
     >
-      <l-tile-layer :url="map"></l-tile-layer>
-      <l-tile-layer :url="weatherMap"></l-tile-layer>
+      <l-tile-layer :url="`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`"></l-tile-layer>
+      <l-tile-layer :url="`https://tile.openweathermap.org/map/precipitation/{z}/{x}/{y}.png?appid=a243fbc125c986c3dd41cc3ae886796d`"></l-tile-layer>
     </l-map>
   </div>
 </template>
@@ -32,8 +32,8 @@
           "https://tile.openweathermap.org/map/precipitation/{z}/{x}/{y}.png?appid=a243fbc125c986c3dd41cc3ae886796d",
         weatherAPI:
           "http://api.openweathermap.org/data/2.5/weather?zip=174-0073,jp&appid=a243fbc125c986c3dd41cc3ae886796d",
-        zoom: 3,
-        center: [47.41322, -1.219482],
+        zoom: 10,
+        center: [35.75, 139.69],
         bounds: null,
       };
     },
@@ -51,15 +51,12 @@
         // return this.wData.weather[0].description
       },
     },
+    watch: {
+      changeCoords: function() {
+        this.center[this.$store.getCoordLon, this.$store.getCoordLon]
+      }
+    },
     mounted() {
-      fetch(this.weatherAPI)
-        .then((resp) => {
-          return resp.json();
-        })
-        .then((data) => {
-          // this.$data.wData = data
-          this.wData = data;
-        });
     },
   };
 </script>
