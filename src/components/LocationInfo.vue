8f7a2dfdaef41f14aa6d160d1e27fbee3ca41a7f
Allen <allenclark@u.boisestate.edu>
2020-08-01 18:07:25 +0900

    working location printing

---
+++ src/components/LocationInfo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="loc">
+    <template v-for="(data, i) in updateLocationInfo">
+      <h4 class="loc-print" :key="i"> {{ data }} <slot v-if="data && i !== 'area'"> 、</slot></h4>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LocationInfo",
+  props: {
+    locationData: {
+      prefecture: String,
+      city: String,
+      area: String
+    }
+  },
+  data () {
+    return {
+      location: {}
+    }
+  },
+  methods: {
+    printLocation: function () {
+      return this.location
+    }
+  },
+  computed: {
+    updateLocationInfo: function () {
+      this.location = this.$props.locationData
+      return this.location
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .loc-print {
+    display: inline-block;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
     <b-row>
-      <b-col fluid>[city.info]</b-col>
+      <b-col fluid> Weather forcast for: </b-col>
+    </b-row>
+    <b-row>
+      <b-col fluid> <LocationInfo :locationData="updateLocationData" /> </b-col>
     </b-row>
     <b-row>
       <b-col>[weather.cards]</b-col>
@@ -9,13 +12,11 @@
     <b-row>
       <b-col>
         <b-card>
-          <WMap />
+          <WMap :wMap="mapWeather()" :bMap="mapBase()" />
         </b-card>
       </b-col>
       <b-col>
-        <b-card>
-          [free.space]
-        </b-card>
+        <b-card>[free.space]</b-card>
       </b-col>
     </b-row>
   </div>
@@ -23,11 +24,27 @@
 
 <script>
   import WMap from "@/components/WMap.vue";
+  import apiConfig from "../../api.config";
+  import LocationInfo from "@/components/LocationInfo.vue"
 
   export default {
     name: "Home",
     components: {
       WMap,
+      LocationInfo
     },
+    methods: {
+      mapWeather: function () {
+        return this.$store.getters.getMapWeather
+      },
+      mapBase: function () {
+        return this.$store.getters.getMapBase
+      },
+    },
+    computed: {
+      updateLocationData: function () {
+        return this.$store.getters.getLocationData
+      }
+    }
   };
 </script>
